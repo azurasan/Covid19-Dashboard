8ea98ff42f8dd4437e5509688b81a5bf359efd36
azurasan <ivanjenius82@gmail.com>
2021-05-10 08:44:22 +0700

    add piechart to countries data

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ const countryTable = document.querySelector("#country tbody");
 const paginationEl = document.querySelector(".pagination-wrapper");
 const countryChart = document.querySelector("#countryChart");
 const spinner = document.querySelector(".loading");
-
+let countryPieChart;
+// Pagination
 let currentPage = 1;
 let rows = 10;
 
@@ -38,10 +39,12 @@ document.addEventListener("click", async function (e) {
 		let countryname = e.target.dataset.countryname;
 		let threeletter = e.target.dataset.threeletter;
 		let detail = await getDetailCoountry(countryname, threeletter);
+		pieChartCountry(countryChart, detail);
 		updateUIModal(detail);
 
 		const closeModalBtn = document.querySelector(".close span");
 		closeModalBtn.addEventListener("click", function () {
+			countryPieChart.destroy();
 			// loading state
 			const numbers = document.querySelectorAll(".number");
 			numbers.forEach((n) => (n.innerText = "Loading..."));
@@ -170,7 +173,6 @@ function getDetailCoountry(countryName, threeLetterSymbol) {
 }
 
 function updateUIModal(data) {
-	// console.log(data);
 	const modalTitle = document.querySelector(".modal-title");
 	modalTitle.innerHTML = data.Country;
 
@@ -223,6 +225,39 @@ function updateUIModal(data) {
 
 function pieChart(wrapper, data) {
 	let worldPieChart = new Chart(wrapper, {
+		type: "pie",
+		data: {
+			labels: ["Total Deaths", "Total Recovered", "Active Cases"],
+			datasets: [
+				{
+					label: "The condition of all cases in the world",
+					data: [data.TotalDeaths, data.TotalRecovered, data.ActiveCases],
+					backgroundColor: [
+						"rgb(220, 53, 69)",
+						"rgb(40, 167, 69)",
+						"rgb(255, 205, 86)",
+					],
+					hoverOffset: 4,
+				},
+			],
+		},
+		options: {
+			plugins: {
+				title: {
+					display: true,
+					text: "The condition of all cases in the world",
+					padding: {
+						top: 20,
+						bottom: 15,
+					},
+				},
+			},
+		},
+	});
+}
+
+function pieChartCountry(wrapper, data) {
+	countryPieChart = new Chart(wrapper, {
 		type: "pie",
 		data: {
 			labels: ["Total Deaths", "Total Recovered", "Active Cases"],
